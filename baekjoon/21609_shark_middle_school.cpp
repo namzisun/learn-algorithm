c7ce276c9c998cca0eb3cbfc32f3c2e7736019e4
namzisun <namzisun@gmail.com>
2024-04-08 01:49:27 +0900

    input

--- baekjoon/21609_shark_middle_school.cpp
+++ baekjoon/21609_shark_middle_school.cpp
@@ -1,5 +1,26 @@
 #include <iostream>
 
-int main() {
+using namespace std;
+
+int dir_r[4] = {-1, 1, 0, 0};
+int dir_c[4] = {0, 0, -1, 1};
+int stage[20][20];
+int N, M;
+
+void makeBlockGroup() {
 	
+}
+
+void autoPlay() {
+
+}
+
+int main() {
+	cin >> N >> M;
+
+	for (int i = 0; i < N; ++i) {
+		for (int j = 0; j < N; ++j) {
+			cin >> stage[i][j];
+		}
+	}
 }
