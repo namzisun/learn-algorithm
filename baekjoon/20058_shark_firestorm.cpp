539d2849d1503ef3bfd2c170fec3369459e3bfd3
namzisun <namzisun@gmail.com>
2024-04-09 22:20:07 +0900

    ..

--- baekjoon/20058_shark_firestorm.cpp
+++ baekjoon/20058_shark_firestorm.cpp
@@ -8,6 +8,28 @@ int map_size;
 int maps[64][64];
 int L[1000];
 
+int first_row;
+int last_row;
+int first_col;
+int last_col;
+
+int a = 0;
+
+void tornado(int l) {
+	while(true) {
+		if (map_size <= first_row || map_size <= first_col 
+			|| map_size <= last_row || map_size <= last_row) break;
+		first_row = l*a;
+		last_row = first_row + l - 1;
+		first_col = l*a;
+		last_col = 
+	}
+}
+
+void firestorm() {
+
+}
+
 int main() {
 
 	cin >> N >> Q;
@@ -21,6 +43,10 @@ int main() {
 	
 	for (int i = 0; i < Q; ++i) {
 		cin >> L[i];
+	}
+
+	for (int i = 0; i < Q; ++i) {
+		tornado(pow(2, L[i]));
 	}
 
 	return 0;
