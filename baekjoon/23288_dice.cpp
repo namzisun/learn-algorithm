a3626ded069b154aaf1c804e9d0af48c4f50bfc1
namzisun <namzisun@gmail.com>
2024-04-13 14:54:21 +0900

    통과

--- baekjoon/23288_dice.cpp
+++ baekjoon/23288_dice.cpp
@@ -17,22 +17,60 @@ int score = 0;
 
 int dice = 6;
 
-int diceArr[6][4] = {
-	{4, 5, 3, 2},
-	{3, 1, 4, 6},
-	{6, 5, 1, 2},
-	{1, 5, 6, 2},
-	{3, 6, 4, 1},
-	{3, 2, 4, 5}
-};
+int diceArr[6] = {2, 4, 1, 3, 5, 6};
+
+// int diceArr[6][4] = {
+// 	{4, 5, 3, 2},
+// 	{4, 1, 3, 6},
+// 	{2, 1, 5, 6},
+// 	{5, 1 ,2, 6},
+// 	{3, 1, 4, 6},
+// 	{3, 5, 4, 2}
+// };
 
 bool isValid(int r, int c) {
 	if (r < 0 || c < 0 || N <= r || M <= c) return false;
 	return true;
 }
 
-int setDice() {
-	return diceArr[dice-1][d];
+void setDice() {
+	int tmp[6] ={0, };
+
+	if (d == 0) {
+		tmp[0] = diceArr[0];
+		tmp[1] = diceArr[5];
+		tmp[2] = diceArr[1];
+		tmp[3] = diceArr[2];
+		tmp[4] = diceArr[4];
+		tmp[5] = diceArr[3];
+	} else if (d == 1) {
+		tmp[0] = diceArr[5];
+		tmp[1] = diceArr[1];
+		tmp[2] = diceArr[0];
+		tmp[3] = diceArr[3];
+		tmp[4] = diceArr[2];
+		tmp[5] = diceArr[4];
+	} else if (d == 2) {
+		tmp[0] = diceArr[0];
+		tmp[1] = diceArr[2];
+		tmp[2] = diceArr[3];
+		tmp[3] = diceArr[5];
+		tmp[4] = diceArr[4];
+		tmp[5] = diceArr[1];
+	} else if (d == 3) {
+		tmp[0] = diceArr[2];
+		tmp[1] = diceArr[1];
+		tmp[2] = diceArr[4];
+		tmp[3] = diceArr[3];
+		tmp[4] = diceArr[5];
+		tmp[5] = diceArr[0];
+	}
+	for (int i = 0; i < 6; ++i) {
+		diceArr[i] = tmp[i];
+	}
+
+	dice = diceArr[5];
+	// cout << "dice : " << dice << endl;
 }
 
 void initVisit() {
@@ -44,8 +82,11 @@ void initVisit() {
 }
 
 void setDir() {
+	
 	if (dice > maps[r][c]) d++;
 	else if (dice < maps[r][c]) d--;
+	while(d < 0) d += 4;
+	// cout << "dir : " << d << endl;
 }
 
 int countScore() {
@@ -55,7 +96,7 @@ int countScore() {
 
 	int number = maps[r][c];
 	int count = 1;
-
+			// cout << "(" << r <<", " << c << ")" << endl;
 	while (!q.empty()) {
 		// cout << "hehe\n";
 		int old_r = q.front().first;
@@ -74,9 +115,10 @@ int countScore() {
 			visit[new_r][new_c] = true;
 			q.push(make_pair(new_r, new_c));
 			count++;
+			// cout << "(" << new_r <<", " << new_c << ")" << endl;
 		}
 	}
-	return count;
+	return count * number;
 }
 
 void move_dice() {
@@ -96,6 +138,10 @@ void move_dice() {
 	r = new_r;
 	c = new_c;
 	d = new_d;
+
+		// 	cout << "r : " << r << endl;
+		// cout << "c : " << c << endl;
+		// cout << "d : " << d << endl;
 
 	// cout << "1\n";
 	// dice = setDice(d);
@@ -121,10 +167,12 @@ int main() {
 		// cout << "d : " << d << endl;
 
 		move_dice();
+		// cout << "======= move dice =======\n";
 		// cout << "r : " << r << endl;
 		// cout << "c : " << c << endl;
 		// cout << "d : " << d << endl;
-
+		// cout << "maps : " << maps[r][c] << endl;
+		// cout << "=========================\n";
 		score += countScore();
 		// cout << "r : " << r << endl;
 		// cout << "c : " << c << endl;
@@ -138,8 +186,8 @@ int main() {
 		// cout << "4\n";
 		
 	}
+		cout << score << '\n';
 
-	cout << score << '\n';
 
 	return 0;
 }
